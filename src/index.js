62b8779b35ff30e591df57bfc5f8bf133420402f
Gla3nat <0207natalya@gmail.com>
2025-11-28 12:37:25 +0600

    add slider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal.js';
 import calc from './modules/calc.js';
 import valid from './modules/valid.js';
 import tabs from './modules/tabs.js';
+import slider from './modules/slider.js';
 
 
 timer('24 november 2025');
@@ -12,4 +13,5 @@ modal();
 calc();
 tabs();
 valid();
+slider();
 
