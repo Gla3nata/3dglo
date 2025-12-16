9de08c82482f4e9b3f345a073b804bbf9f1c6cfa
Gla3nat <0207natalya@gmail.com>
2025-12-16 23:10:10 +0600

    закончила урок

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,33 +1,39 @@
-const sendForm = ({formId , someElem = [] }) => {
+const sendForm = ({ formId, someElem = [] }) => {
     const form = document.getElementById(formId)
+    const statusBlock = document.createElement('div')
+    const loadText = 'Загрузка'
+    const errorText = 'Ошибка'
+    const successText = 'Спасибо. Наш менеджер свяжется с Вами!'
+
 
     const validate = (list) => {
-        let success = true 
+        let success = true
 
-        list.forEach(input => {
-            if (!input.classList.contains('success')) {
-                success = false
-            }
-        })
+        // list.forEach(input => {
+        //     if (!input.classList.contains('success')) {
+        //         success = false
+        //     }
+        // })
         return success
     }
 
     const sendData = (data) => {
         return fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
-             body: JSON.stringify(data),
+            body: JSON.stringify(data),
             headers: {
                 "Content-Type": "application/json"
             }
         }).then(res => res.json())
     }
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-
-        const formElements = form.querySelectorAll('input')
+    const submitForm = () => {
+         const formElements = form.querySelectorAll('input')
         const formData = new FormData(form)
         const formBody = {}
+
+        statusBlock.textContent = loadText
+        form.append(statusBlock)
 
         formData.forEach((val, key) => {
             formBody[key] = val
@@ -38,22 +44,41 @@ const sendForm = ({formId , someElem = [] }) => {
 
             if (elem.type === 'block') {
                 formBody[elem.id] = element.textContent
-            } else if (elem.type === 'input'){
+            } else if (elem.type === 'input') {
                 formBody[elem.id] = element.value
             }
         })
 
         console.log('sub');
 
-        if (validate(formElements)){
-            sendData(formBody).then(data => {
-            console.log(data);
-        })
-        } else{
+        if (validate(formElements)) {
+            sendData(formBody)
+                .then(data => {
+                    statusBlock.textContent = successText
+                    formElements.forEach(input => {
+                        input.value = ''
+                    })
+                })
+                .catch(error => {
+                    statusBlock.textContent = errorText
+                })
+        } else {
             alert('заполнить данные')
         }
 
-        
+    }
+
+    try{
+        if(!form){
+            throw new Error ('Верните форму на место')
+        }
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        submitForm()
     })
+    }catch(error){
+        console.log(error.message)
+    }
 }
 export default sendForm;
